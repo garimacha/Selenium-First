00614613ab01bbbd6f21fdba5a7658dd5813f361
Garima Chandra <garich01@noa.nintendo.com>
2016-10-18 18:33:27 -0700

    added more data

--- src/main/java/NavigateToUrl.java
+++ src/main/java/NavigateToUrl.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
@@ -7,6 +9,12 @@ public class NavigateToUrl {
     public static void main(String[] args){
         WebDriver driver=new FirefoxDriver();
         driver.get("https://www.google.com");
+       // WebElement searchBox =
+
+        WebElement searchbox= driver.findElement(By.name("q"));
+                searchbox.sendKeys("Hello Darling");
+        searchbox.submit();
+
 
 
 
