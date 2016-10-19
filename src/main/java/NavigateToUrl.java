9c0fa4c7659de77e831dd929546792aae1df6c28
worryNerd <dgarima10@gmail.com>
2016-10-18 18:27:31 -0700

    Update NavigateToUrl.java

--- src/main/java/NavigateToUrl.java
+++ src/main/java/NavigateToUrl.java
@@ -1,9 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-/**
- * Created by garich01 on 10/18/2016.
- */
+
 public class NavigateToUrl {
 
     public static void main(String[] args){
